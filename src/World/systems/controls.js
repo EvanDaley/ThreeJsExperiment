760f794e36f6118607e40ad4da9056a5fcc619c0
Evan <daley.evan@gmail.com>
2025-05-16 21:57:25 -0700

    Initial commmit

---
+++ src/World/systems/controls.js
@@ -0,0 +1,20 @@
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+function createControls(camera, canvas) {
+  const controls = new OrbitControls(camera, canvas);
+
+  controls.enableDamping = true;
+
+  // forward controls.update to our custom .tick method
+  controls.tick = () => {
+    controls.update()
+  }
+
+  controls.enableRotate = false;
+  controls.enablePan = false;
+  controls.enableZoom = false;
+
+  return controls;
+}
+
+export { createControls };
